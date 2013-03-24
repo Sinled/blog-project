ed137a11ea463887fb6903048cbbf0c38efc6d28
DDovgan <sinled@gmail.com>
2013-03-24 11:33:19 +0200

    some fixes

--- blog_project/blog_project/views.py
+++ blog_project/blog_project/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse
 from django.views.generic.simple import direct_to_template
 
+
 def news(request):
-  return direct_to_template(request, "news.html")
+    return direct_to_template(request, "news.html")
+
 
 def blog(request):
-  return direct_to_template(request, "blog.html")
-  
+    return direct_to_template(request, "blog.html")
+
+
 def contacts(request):
-  return direct_to_template(request, "contacts.html")
+    return direct_to_template(request, "contacts.html")
