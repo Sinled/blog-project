1cc34cb0bd2c2215bf55e51871584ba720e7150a
DDovgan <sinled@gmail.com>
2013-03-24 18:50:59 +0200

    refactoring

--- blog_project/blog_project/urls.py
+++ blog_project/blog_project/urls.py
@@ -7,13 +7,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'blog_project.views.news', name='news'),
-    url(r'^blog/$', 'blog_project.apps.blog.views.blog', name='blog'),
-    url(r'^blog_post/(?P<post_id>\d+)/$', 'blog_project.apps.blog.views.blog_post', name='blog_post'),
     url(r'^contacts/$', 'blog_project.views.contacts', name='contacts'),
-    # url(r'^blog_project/', include('blog_project.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+    url(r'^blog/', include('blog_project.apps.blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
